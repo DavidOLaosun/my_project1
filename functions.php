d8443d5e9e87b071de1a56558b851d222c9cd788
DavidOLaosun <54552473+DavidOLaosun@users.noreply.github.com>
2019-11-11 14:32:50 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,82 +1,61 @@
 <?php
-// PHP - Random Quote Generator
-//Multidimensional array for quote elements
-//Inner associative array elements
-  $quotes[] = [
+// PHP - Random Quote Generator 
+// Multidimesional array for quote elements 
+// Inner associative array elements
+$quotes = []; 
 
 $quotes[]= [
-
-  'quote'=>"God may allow us at times to hit rock bottom,to show us He's the rock-at the bottom.",
-  'source'=>"Kirk Franklin",
-  'citation'=>"AZ Quotes",
-  'tag'=>"Gospel Singers",
- 
+    'quote'=>"God may allow us at times to hit rock bottom, to show us He's the rock at the bottom.",
+    'source'=>"Kirk Franklin",
+    'citation'=>"AZ Quotes",
+    'tag'=>"Gospel Singers",
+];
+$quotes[]=[
+    'quote'=>"Focusing on the negative only makes a difficult journey more difficult.",
+    'source'=>"TobyMac",
+    'citation'=>"AZ Quotes",
+    'tag'=>"Gospel Singers",
+];
+$quotes[]=[
+    'quote'=>"We may have pain, but not as great as his name.",
+    'source'=>"Jamie Grace",
+    'citation'=>"AZ Quotes",
+    'tag'=>"Gospel Singers",
+];
+$quotes[]=[
+    'quote'=>"Where God isn't moved by the quality of our voice but by the condition of our hearts.",
+    'source'=>"Chris Tomlin",
+    'citation'=>"AZ Quotes",
+    'tag'=>"Gospel Singers",
 ];
 
-$quotes[]= [
-  
-  'quote'=>"Focusing on the negative only makes a difficult journey more difficult.",
-  'source'=>"TobyMac"
-  'citation'=>"AZ Quotes",
-  'tag'=>"Gospel Singer",
-
-];
-
-$quotes[]= [
-
-  'quotes'=>"We may have pain,but not as great as His name.",
-  'source'=>"Jamie Grace",
-  'citation'=>"AZ Quotes",
-  'tag'=>"Gospel Singer",
-
-];
-
-$quotes[]= [
-  
-  'quotes'=>"Where God isn't moved by the quality of our voice but by the condition of our hearts.",
-  'source'=>"Chris Tomlin",
-  'citation'=>"AZ Quotes",
-  'tag'=>"Gospel Singers",
-
-];
-
-$quotes[]= [
-
-  'quotes'=>"You are holy,great and mighty,the moon and stars DECLARE who You are...I'm so unworthy,but still You love me,forever my heart will sing of how great You are!",
-  'source'=>"Phil Wickham",
-  'citation'=>"AZ Quotes",
-  'tag'=>"Gospel Singers
-
+$quotes[]=[
+    'quote'=>"You are holy,great and mighty,the moon and stars DECLARE who You are...I'm so unworthy,but still You love me,forever my heart will sing of how great You are!",
+    'source'=>"Phil Wickham",
+    'citation'=>"AZ Quotes",
+    'tag'=>"Gospel Singers",
 ];
 
 //Create a function called getRandomQuote
+function getRandomQuote($quotes){
+    $randomNum=rand(0,4);
 
-function getRandomQuote($array){
+    //Passing on the randomly selected inner associative array
+    $array = quotes[$randomNum];
 
-  $randomNum=rand(0,24);
-  //Passing on the randomly selected inner associative array 
-
-  return $array[$randomNum];
-
+    return $array;
 }
 
-//printQuote function will print the returned quote from the getRandomQuote function 
+//printQuote function will print the returned quote from the getRandomQuote function
+function printQuote($quotes){
+    //global $quotes;
+    $aQuote = getRandomQuote($quotes);
 
-function printQuote(){
-  global $quotes;
- $aQuote= getRandomQuote($quotes);
+    $quoteOutput='';
 
-  $quoteOutput='';
-
-if($aQuote['citation'] !='' ||$aQuote['source'] !='' ||$aQuote['tag'] !=''){
-
-$quoteOutput="<p class='quote'>" . $aQuote=['quote'] . "</p>;
-$quoteOutput="<p class='source'>" . $aQuote=
-
-
-
-
-
-
-
-
+    if($aQuote['citation'] !='' || $aQuote['source'] !='' || $aQuote['tag'] !=''){
+        $quoteOutput = "<p class='quote'>" . $aQuote['quote'] .  "</p>";
+        $quoteOutput .="<p class='source'>" . $aQuote['source'];
+    }
+    return $quoteOutput;
+}
